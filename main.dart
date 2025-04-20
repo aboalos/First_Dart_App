31b6c262f2d9ade1f4752469c20511e5370bd2c1
aboalos <aobeid09@gmail.com>
2025-04-20 21:44:43 +0300

    update

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main ()
+{
+String myname = 'Osama Obeid';
+int myAge = 23;
+int myyear = 2001;
+double myHight = 190.5;
+
+print(myname[0].toUpperCase());
+print (myHight.ceil());
+print (myHight.floor());
+print (myHight.round());
+
+}
